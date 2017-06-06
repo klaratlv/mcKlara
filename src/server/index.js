730e946be6ed3170403fd0fbab269abdd0b99658
Amit Avihud <amit.avihud@klarna.com>
2017-06-06 17:00:14 +0300

    Trying to solve blynk dns problem

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 const blynkToken = '70bfe532b9a84f89a889e0c7dfad386f'
 
-// dns.setServers(['8.8.8.8', '8.8.4.4'])
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 let blynkIp;
 dns.resolve('blynk-cloud.com', (err, res) => {blynkIp=res[0]})
 
@@ -23,11 +23,15 @@ app.get('/send', (req, res) => {
   axios.get(`http://${blynkIp}/${blynkToken}/update/V31`, {
     params: {
       value: `${params.station},${params.orientation},${params.mission},${params.param1},${params.param2},${params.param3}`
+    },
+    headers: {
+      'Host': 'blynk-cloud.com'
     }
   })
   .then(function (response) {
     console.log(response);
-    res.json(response)
+    // res.json(response)
+    res.json({horray: 1})
   })
   .catch(function (error) {
     console.log(error);
